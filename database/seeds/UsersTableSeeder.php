ffe3e3a620daf84402167f4c2df303fedf9178c6
rizkiaprilan <riskiazza@gmail.com>
2019-11-23 23:05:54 +0700

    “Log_Out_undermaintenance“

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            ['name'=> 'Mhd. Rezki Aprilan','email'=>'riskiazza@gmail.com','password'=>'123456789','gender'=>'male','address'=>'jl. Nimun Raya','role'=>'admin','date'=>'1999-04-08','photo'=>'default.jpg'],
+            ['name'=> 'Kunta Rizki Purnama','email'=>'kuntarizki@gmail.com','password'=>'123456789','gender'=>'male','address'=>'jl. Cempedak','role'=>'admin','date'=>'1999-08-08','photo'=>'default.jpg']
+        ];
+        foreach ($data as $d){
+            DB::table('users')->insert([
+                'name' => $d['name'],
+                'email' => $d['email'],
+                'password' => Hash::make($d['password']),
+                'gender' => $d['gender'],
+                'role'=> $d['role'],
+                'address' => $d['address'],
+                'date' => $d['date'],
+                'photo' => $d['photo'],
+            ]);
+        }
+
+
+
+
+    }
+}
